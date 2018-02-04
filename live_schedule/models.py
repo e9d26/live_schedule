b0988b1dbd9b41028fae9f1589fb09bd1e4f3d4a
e9d26 <osanai.hiroki.26@gmail.com>
2018-02-04 10:02:14 +0000

    modify table definition

--- live_schedule/models.py
+++ live_schedule/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Schedule(models.Model):
-    date = models.DateTimeField()
-    start_at = models.DateTimeField()
+    date = models.DateField()
+    start_at = models.TimeField()
     title = models.CharField(max_length=200)
     place = models.CharField(max_length=200)
     text = models.TextField()
