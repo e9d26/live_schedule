41d9fed7c7d09b710a0d1624640e189f37ad13f3
e9d26 <osanai.hiroki.26@gmail.com>
2018-02-07 15:09:33 +0000

    modify webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -6,12 +6,44 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'lxml')
 schedule_pc = soup.find("div", {"id": "schedule_pc"})
 contents = schedule_pc.find_all('ul')
-for content in contents:
-    time = content.find(re.compile("\d"))
-    with open('test.text', 'a') as file:
-        file.write(str(time))
 
 schedule = {}
+for content in contents:
+    body = str(content)
+    timepattern = re.compile('(?<=開演)\d{2}:\d{2}')
+    namepattern = re.compile('(?<=公演名</div>\n<div class="schedule_con">)[^<]*')
+#    memberpattern = re.compile('(?<=<div class="day_con_name">\n).{10}')
+#    day = content.find(id="dayTitle")
+#    day = content.find("li", {"id": "dayTitle"})
+    time = timepattern.search(body)
+    name = namepattern.search(body)
+    member = memberpattern.search(body)
+#    member = content.find("div", {"class": "day_con_name"})
+    schedule['time'] = time.group()
+    schedule['name'] = name.group()
+#    schedule['member'] = member.group()
+#    schedule['member'] = member.string
+    with open('regex.text', 'a') as file:
+#        file.write(day.string)
+        file.write(str(schedule))
+
+    print(schedule)
+
+
+    with open('regex.text', 'a') as file:
+#        file.write(day.string)
+        file.write(time.group())
+        file.write(name.group())
+        file.write(member.string)
+
+
+
+    print(time)
+    print(name)
+
+
+
+
 
 
 topics = bs.select('.fl, .fr')
